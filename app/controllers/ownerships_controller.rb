6f9eb67fefdd07f5f478483f9ae7ec588f89aa42
hiroshi_kurita <furitahiroshi@gmail.com>
2015-12-13 09:37:20 +0000

    modification of response_group

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -13,8 +13,7 @@ class OwnershipsController < ApplicationController
       begin
       
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        #response = {}
-        response = Amazon::Ecs.item_lookup(params[:asin], :country => 'jp')
+        response = Amazon::Ecs.item_lookup(params[:asin], :country => 'jp', :response_group => "Medium")
         
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
@@ -29,30 +28,27 @@ class OwnershipsController < ApplicationController
       @item.raw_info        = amazon_item.get_hash
       @item.save!
     end
-
+    
+    # TODO ユーザにwant or haveを設定する
+    # params[:type]の値ににHaveボタンが押された時には「Have」,
+    # Wantボタンがされた時には「Want」が設定されています。
     if params[:type] == "Want"
       current_user.want(@item)
     elsif params[:type] == "Have"
       current_user.have(@item)
     end
     
-    # TODO ユーザにwant or haveを設定する
-    # params[:type]の値ににHaveボタンが押された時には「Have」,
-    # Wantボタンがされた時には「Want」が設定されています。
-    
   end
 
   def destroy
+    # TODO 紐付けの解除。 
+    # params[:type]の値ににHavedボタンが押された時には「Have」,
+    # Wantedボタンがされた時には「Want」が設定されています。
     @item = Item.find(params[:item_id])
     if params[:type] == "Want"
       current_user.unwant(@item)
     elsif params[:type] == "Have"
       current_user.unhave(@item)
     end
-
-    # TODO 紐付けの解除。 
-    # params[:type]の値ににHavedボタンが押された時には「Have」,
-    # Wantedボタンがされた時には「Want」が設定されています。
-
   end
 end
