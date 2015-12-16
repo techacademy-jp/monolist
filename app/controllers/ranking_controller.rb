0b3ca23461009c9fbc68f6947feb58dc02655a97
hiroshi_kurita <furitahiroshi@gmail.com>
2015-12-16 11:50:33 +0000

    modification of sort_by

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,13 +2,13 @@ class RankingController < ApplicationController
   def have
     @ranking = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
     item_ids = @ranking.keys
-    @items = Item.find(item_ids).sort_by{|o| @ranking.index(o.id)}.reverse
+    @items = Item.find(item_ids).sort_by{|o| item_ids.index(o.id)}
   end
 
   def want
     @ranking = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
     item_ids = @ranking.keys
-    @items = Item.find(item_ids).sort_by{|o| @ranking.index(o.id)}.reverse
+    @items = Item.find(item_ids).sort_by{|o| item_ids.index(o.id)}
   end
 
 end
